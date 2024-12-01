6617444f2d94dc45b829b9cf28eeba62dc8d2838
dainji04 <ndai31072004@gmail.com>
2024-12-01 15:27:07 +0700

    loading state

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <div>
+            <routerView />
+        </div>
+    </div>
+</template>
---
+++ src/views/home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div v-if="loading" class="loading-screen">
+            <Loading />
+        </div>
+        <div class="home">
+            <h1>this is home page</h1>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Loading from '@/components/loading.vue';
+const loading = ref(true);
+
+const load = setTimeout(() => {
+    loading.value = false;
+}, 4000);
+</script>
+
+<style>
+.loading-screen {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-size: 24px;
+}
+</style>
